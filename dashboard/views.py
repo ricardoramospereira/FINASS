728476861e0e917bd29df964192a18343684a45a
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-08-10 13:57:59 -0300

    bd

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 @login_required(login_url='/auth/login')
 def index(request):
-    return render(request, 'dashboard/index.html')
+    if request.method == "GET": # test
+        return render(request, 'dashboard/index.html')
