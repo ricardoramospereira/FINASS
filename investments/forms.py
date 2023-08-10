9a9149ca75a472a0db433f4b16cfd3a13bcf1115
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-08-10 12:34:21 -0300

    crud investiment

--- investments/forms.py
+++ investments/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
 from .models import Investment
+from django import forms
 
 class InvestmentForm(ModelForm):
     class Meta:
         model = Investment
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'required': 'required'}),
+            'value': forms.TextInput(attrs={'required': 'required'}),
+            # Adicione outros campos aqui, se necessário
+        }
