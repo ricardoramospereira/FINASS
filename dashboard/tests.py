9812f23fc5b2e7d79d7a6387c07ae39832b239fe
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-09-05 19:03:15 -0300

    pytest

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class DashboardURLsTest(TestCase):
+    def test_dashboard_url_is_correct(self):
+        index_url = reverse('index')
+        self.assertEqual(index_url, '/')
