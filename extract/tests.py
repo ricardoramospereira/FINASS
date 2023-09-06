54b41afb80ac57ee2587dfbcf6600e9a00a9ce41
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-09-06 09:40:13 -0300

    organization for tests

--- extract/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-# Create your tests here.
-class ExtractURLsTest(TestCase):
-    def test_extract_url_is_correct(self):
-        self.ass
